2617b20c658b49f5855f49cecb872a340338ac1b
shrutika1195-star <shrutika1195@gmail.com>
2025-12-17 10:37:53 +0530

    Practiceproject
    Practiceproject for pull

---
+++ PracticeProject/src/pullproject/Pull.java
@@ -0,0 +1,24 @@
+package pullproject;
+
+public class Pull {
+
+	public static void main(String[] args) {
+	String[] fruit = new String [10];
+		
+		fruit[0]="Mango";
+		fruit[1]="Apple";
+		fruit[2]="Banana";
+		fruit[3]="Orange";
+		fruit[4]="Cherry";
+		fruit[5]="Pineapple";
+		fruit[6]="Grapes";
+		fruit[7]="Strawberries";
+		fruit[8]="Avacados";
+		fruit[9]="Blueberry";
+		for(String b:fruit) {
+			System.out.println(b);
+		}
+
+	}
+
+}
